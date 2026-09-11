4434eda6df476955b0d8bbeedcee8b2058ebb865
AXLAAF <133803971+AXLAAF@users.noreply.github.com>
2026-09-10 18:29:11 -0600

    Completar main de Biblioteca

--- Actividad4/Biblioteca.java
+++ Actividad4/Biblioteca.java
@@ -1,9 +1,11 @@
 
-//se llamaran los metodos de los otras clases para contemplar la modularidad
-
 public class Biblioteca {
     public static void main(String[] args) {
-
-    
+        Libro libro = new Libro("El Quijote", "Miguel de Cervantes", "978-8408077947", 1605);
+        Usuario usuario = new Usuario("Juan Pérez", "juan@email.com", 1);
+        
+        libro.mostrarLibro();
+        System.out.println();
+        usuario.mostrarUsuario();
     }
 } 
