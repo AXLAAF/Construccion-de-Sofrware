9eabbc2ed004d232de2228e9c0fa7d2b4cc8dbd5
AXLAAF <133803971+AXLAAF@users.noreply.github.com>
2026-09-10 18:27:52 -0600

    Update Biblioteca.java
    Added comments for modularity and method calls.

--- Actividad4/Biblioteca.java
+++ Actividad4/Biblioteca.java
@@ -1,5 +1,9 @@
+
+//se llamaran los metodos de los otras clases para contemplar la modularidad
+
 public class Biblioteca {
     public static void main(String[] args) {
-        
+
+    
     }
 } 
