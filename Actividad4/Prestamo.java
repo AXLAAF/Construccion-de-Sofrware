dd10612012665d66304581aacd605bbb9ed81c5b
Diego-Ceron <A20203048@alumnos.uady.mx>
2026-09-10 18:19:20 -0600

    cosntructor prestamo

--- Actividad4/Prestamo.java
+++ Actividad4/Prestamo.java
@@ -8,6 +8,13 @@ public class Prestamo {
     private double multaPorDia; 
     private int diasRetraso;
 
+    public Prestamo(String fechaPrestamo, String fechaDevolucion, double multaPorDia, int diasRetraso) {
+        this.fechaPrestamo = fechaPrestamo;
+        this.fechaDevolucion = fechaDevolucion;
+        this.multaPorDia = multaPorDia;
+        this.diasRetraso = diasRetraso;
+    }
+
      public double calcularMulta() { 
         return multaPorDia * diasRetraso; 
     }
