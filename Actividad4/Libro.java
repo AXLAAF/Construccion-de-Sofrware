e91f5fcd068837c05f529836a8099810b6374d86
Diego-Ceron <A20203048@alumnos.uady.mx>
2026-09-10 18:17:45 -0600

    constructor libro

--- Actividad4/Libro.java
+++ Actividad4/Libro.java
@@ -4,6 +4,13 @@ public class Libro {
     private String isbnLibro; 
     private int anioPublicacion;
 
+    public Libro(String tituloLibro, String autorLibro, String isbnLibro, int anioPublicacion) {
+        this.tituloLibro = tituloLibro;
+        this.autorLibro = autorLibro;
+        this.isbnLibro = isbnLibro;
+        this.anioPublicacion = anioPublicacion;
+    }
+
     public void mostrarLibro() { 
         System.out.println("Título: " + tituloLibro); 
         System.out.println("Autor: " + autorLibro); 
